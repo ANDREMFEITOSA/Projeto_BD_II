ff9bc1bd86ce3266a86045fd63702cf400caaef4
andre feitosa <andremfeitosa@hotmail.com>
2022-10-25 17:26:54 -0300

    subindo projeto

---
+++ src/main/java/conexao/ConectaHibernateMysql.java
@@ -0,0 +1,10 @@
+package conexao;
+import org.hibernate.Session;
+public class ConectaHibernateMysql {
+	public static void main(String[] args) {
+		Session sessao = null;
+		sessao = HibernateUtil.getSessionFactory().openSession();
+		System.out.println("Conectou!");
+		sessao.close();
+	}
+}
