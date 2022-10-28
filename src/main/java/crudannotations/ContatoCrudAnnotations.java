6751b4c9660e6a1c31a620a1d2c25599c0c61693
andre feitosa <andremfeitosa@hotmail.com>
2022-10-28 00:31:27 -0300

    atualização 01 projeto

--- src/main/java/crudannotations/ContatoCrudAnnotations.java
+++ src/main/java/crudannotations/ContatoCrudAnnotations.java
@@ -1,4 +1,6 @@
 package crudannotations;
+import java.util.ArrayList;
+import java.security.SecureRandom;
 import java.sql.Date;
 import java.util.List;
 import org.hibernate.*;
@@ -31,27 +33,51 @@ public class ContatoCrudAnnotations {
 		Transaction transacao = sessao.beginTransaction(); 
 		ContatoCrudAnnotations contatoCrud = new ContatoCrudAnnotations(sessao);
 
-		Contato contato1 = new Contato();
-		contato1.setNome("Solanu");
-		contato1.setTelefone("(47) 3333-4444");
-		contato1.setEmail("solanu@javaparaweb.com.br");
-		contato1.setDataCadastro(new Date(System.currentTimeMillis()));
-		contato1.setObservacao("Novo cliente");
-		contatoCrud.salvar(contato1); 
-		contato1.setObservacao("Retomar contato");
-		contatoCrud.atualizar(contato1); 
+		int i;
+		int j = 50;
+		
+		List<String> nomes = new ArrayList<String>();
+		nomes.add("Andre");
+		nomes.add("Beatriz");
+		nomes.add("Carlos");
+		nomes.add("Debora");
+		nomes.add("Elias");
+		
+		List<String> telefones = new ArrayList<String>();
+		telefones.add("99999-1111");
+		telefones.add("99999-2222");
+		telefones.add("99999-3333");
+		telefones.add("99999-4444");
+		telefones.add("99999-5555");
 
-		Contato contato2 = new Contato();
-		contato2.setNome("Lunare");
-		contato2.setTelefone("(47) 7777-5555");
-		contato2.setEmail("lunare@javaparaweb.com.br");
-		contato2.setDataCadastro(new Date(System.currentTimeMillis()));
-		contato2.setObservacao("Cliente em dia");
-		contatoCrud.salvar(contato2); 
-
-		System.out.println("Total de registros cadastrados: " + contatoCrud.listar().size()); 
-		contatoCrud.excluir(contato2); 
-		transacao.commit(); 
+		List<String> emails = new ArrayList<String>();
+		emails.add("aaa@xxx.br");
+		emails.add("bbb@xxx.br");
+		emails.add("ccc@xxx.br");
+		emails.add("ddd@xxx.br");
+		emails.add("eee@xxx.br");
+		
+		List<String> obs = new ArrayList<String>();
+		obs.add("obs1");
+		obs.add("obs2");
+		obs.add("obs3");
+		obs.add("obs4");
+		obs.add("obs5");
+		
+		SecureRandom random = new SecureRandom();
+		
+		for(i = 0; i < j; i++) {
+			Contato contato = new Contato();
+			contato.setNome(nomes.get(random.nextInt(nomes.size())));
+			contato.setTelefone(telefones.get(random.nextInt(telefones.size())));
+			contato.setEmail(emails.get(random.nextInt(emails.size())));
+			contato.setDataCadastro(new Date(System.currentTimeMillis()));
+			contato.setObservacao(obs.get(random.nextInt(obs.size())));
+			contatoCrud.salvar(contato);
+		}
+		
+		transacao.commit();
+		 
 		System.out.println("Total de registros cadastrados: " + contatoCrud.listar().size()); 
 	}
 
